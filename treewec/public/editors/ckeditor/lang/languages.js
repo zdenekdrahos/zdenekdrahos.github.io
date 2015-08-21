335df806e1fca5ab3cfa13deed987fbdb33c221c
Zdeněk Drahoš <drahoszdenek@gmail.com>
2015-08-21 15:52:31 +0200

    Middleman build

--- treewec/public/editors/ckeditor/lang/languages.js
+++
@@ -1,6 +0,0 @@
-/*
- Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
-var CKEDITOR_LANGS=function(){var c={cs:"Czech",en:"English"},b=[],a;for(a in CKEDITOR.lang.languages)b.push({code:a,
-name:c[a]||a});b.sort(function(a,b){return a.name<b.name?-1:1});return b}();
